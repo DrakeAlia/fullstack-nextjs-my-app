2b32e59c4956081857f3500106c7f9138771436a
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-03-10 14:57:10 -0800

    GreetingsSkeleton Component

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -2,16 +2,19 @@ import { delay } from "@/lib/async";
 import { getUserFromCookie } from "@/lib/auth";
 import { db } from "@/lib/db";
 import Greeting from "@component/components/Greeting";
+import GreetingsSkeleton from "@component/components/GreetingsSkelton";
 import { cookies } from "next/headers";
 import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function Page() {
   return (
-    <div className="h-full overflow-y-auto pr-6 w-1/1">
+    <div className="h-full overflow-y-auto pr-6 w-full">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
         <div className="flex-1 grow flex">
-          <Greeting />
+          <Suspense fallback={<GreetingsSkeleton />}>
+            <Greeting />
+          </Suspense>
         </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
           {/** projects map here */}
