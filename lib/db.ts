c7cc899875b8b38c3439e7d2790f6ea6ea4c65cc
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-03-12 16:12:35 -0700

    added notes in db.ts file

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
+// Importing out our generated Prisma Client
+
+
+// TS will freak out if we try to use something the in the global scope that's not there
 declare global {
   // eslint-disable-next-line no-var
   var cachedPrisma: PrismaClient;
 }
 
+// Checking to see if we're in a production environment
+// If we are, we'll create a new Prisma Client
+// Else if there isn't one in cache, go ahead and get it and then export that one out
 let prisma: PrismaClient;
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
